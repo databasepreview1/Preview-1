9d04db47b4bb40dabf3d3519e051b92bdf50ea8b
databasepreview1 <databasepreview1@gmail.com>
2026-09-18 23:07:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = os.environ.get("API_HASH", "ffd1404c4d9d69d3417b45c33e32ea6f")
 
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5539467127"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://sanjusen212121:lp5kjPXsxfg9j580@cluster0.j7wrbx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sanjusen212121_db_user:UWIsrpQxKLZYnzdC@cluster0.pwd8jy0.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
